4dc143dbfd3bce013dcc352207c97ff557cc5548
desongyu <desong.e.yu@gmail.com>
2016-10-13 14:58:33 -0400

    add week 3 proj task 1

---
+++ week3/conFusion/app/archive/ngRouter/scripts/controller.js
@@ -0,0 +1,96 @@
+//"use strict";
+
+angular.module('confusionApp')
+
+         .controller('MenuController', ['$scope', 'menuFactory', function($scope, menuFactory) {
+            
+            $scope.tab = 1;
+            $scope.filtText = '';
+            $scope.showDetails = false;
+
+           $scope.dishes= menuFactory.getDishes();
+                        
+            $scope.select = function(setTab) {
+                $scope.tab = setTab;
+                
+                if (setTab === 2) {
+                    $scope.filtText = "appetizer";
+                }
+                else if (setTab === 3) {
+                    $scope.filtText = "mains";
+                }
+                else if (setTab === 4) {
+                    $scope.filtText = "dessert";
+                }
+                else {
+                    $scope.filtText = "";
+                }
+            };
+
+            $scope.isSelected = function (checkTab) {
+                return ($scope.tab === checkTab);
+            };
+    
+            $scope.toggleDetails = function() {
+                $scope.showDetails = !$scope.showDetails;
+            };
+        }])
+
+        .controller('ContactController', ['$scope', function($scope) {
+
+            $scope.feedback = {mychannel:"", firstName:"", lastName:"", agree:false, email:"" };
+            
+            var channels = [{value:"tel", label:"Tel."}, {value:"Email",label:"Email"}];
+            
+            $scope.channels = channels;
+            $scope.invalidChannelSelection = false;
+                        
+        }])
+
+        .controller('FeedbackController', ['$scope', function($scope) {
+            
+            $scope.sendFeedback = function() {
+                
+                console.log($scope.feedback);
+                
+                if ($scope.feedback.agree && ($scope.feedback.mychannel === "")) {
+                    $scope.invalidChannelSelection = true;
+                    console.log('incorrect');
+                }
+                else {
+                    $scope.invalidChannelSelection = false;
+                    $scope.feedback = {mychannel:"", firstName:"", lastName:"", agree:false, email:"" };
+                    $scope.feedback.mychannel="";
+                    $scope.feedbackForm.$setPristine();
+                    console.log($scope.feedback);
+                }
+            };
+        }])
+
+        .controller('DishDetailController', ['$scope', '$routeParams', 'menuFactory', function($scope, $routeParams, menuFactory) {
+
+            //$scope.dish= menuFactory.getDish(3);
+
+            var dish= menuFactory.getDish(parseInt($routeParams.id,10));                        
+            $scope.dish = dish;
+
+        }])
+
+        .controller('DishCommentController', ['$scope', function($scope) {
+            
+            $scope.commentitem={author:"", date:"", rating:"5", comment:""};
+            
+            $scope.submitComment = function () {
+               console.log($scope.commentitem);
+               if ($scope.commentitem.author==="" || $scope.commentitem.comment===""){
+                  console.log('incorrect');
+               }
+               else {
+                  $scope.commentitem.date = new Date().toISOString();
+                  $scope.dish.comments.push($scope.commentitem);
+                  $scope.commentForm.$setPristine();
+                  $scope.commentitem={author:"",date:"",rating:"5", comment:""};
+                  console.log(commentitem);
+               }
+            };
+        }]);
